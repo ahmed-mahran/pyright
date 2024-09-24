d8c1402ea2241f72739a127e82d0b44fa2adc540
Ahmed Mahran <ahmed.mahran@mashin.io>
2024-09-24 19:49:56 +0300

    Extend subscriptable decorator to subscript methods
    https://github.com/ahmed-mahran/mypyright/issues/4

--- packages/pyright-internal/typeshed-fallback/stubs/mypyright-extensions/mypyright_extensions.pyi
+++ packages/pyright-internal/typeshed-fallback/stubs/mypyright-extensions/mypyright_extensions.pyi
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import TYPE_CHECKING, Callable, Concatenate, Type
+from typing import TYPE_CHECKING, Callable, Concatenate, Self, Type
 
 if TYPE_CHECKING:
   from typing import type_check_only
@@ -11,8 +11,11 @@ class _MyPyright(ABC):
 class Map[F, *Ts](_MyPyright):
   ...
 
-class subscriptable[*T, **P, R]:
-  def __init__(self, fn: Callable[Concatenate[Map[Type, *T], P], R]) -> None:
+class subscriptable[Owner, *T, **P, R]:
+  def __init__(self, fn: Callable[Concatenate[Map[Type, *T], P], R] | Callable[Concatenate[Owner, Map[Type, *T], P], R]) -> None:
+    ...
+
+  def __get__(self, instance: Owner, owner: Type[Owner]) -> Self:
     ...
 
   def __getitem__(self, tp: Map[Type, *T]) -> Callable[P, R]:
