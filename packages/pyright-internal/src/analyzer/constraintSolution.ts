ec7dd6c6a268450d65ab968f67053efdc89f76ce
Ahmed Mahran <ahmed.mahran@mashin.io>
2024-10-03 18:29:47 +0300

    Add utility function to combine constraint solutions

--- packages/pyright-internal/src/analyzer/constraintSolution.ts
+++ packages/pyright-internal/src/analyzer/constraintSolution.ts
@@ -9,7 +9,7 @@
  */
 
 import { assert } from '../common/debug';
-import { FunctionType, ParamSpecType, Type, TypeVarType } from './types';
+import { combineTypes, FunctionType, ParamSpecType, Type, TypeVarType } from './types';
 
 // Records the types associated with a set of type variables.
 export class ConstraintSolutionSet {
@@ -48,6 +48,33 @@ export class ConstraintSolutionSet {
             }
         });
     }
+
+    static combine(allSets: ConstraintSolutionSet[]): ConstraintSolutionSet | undefined {
+        if (allSets.length > 1) {
+            const result = new ConstraintSolutionSet();
+            const allKeys: Set<string> = new Set();
+            allSets.forEach((set) => {
+                for (const key of set._typeVarMap.keys()) {
+                    allKeys.add(key);
+                }
+            });
+            for (const key of allKeys) {
+                const types: Type[] = [];
+                allSets.forEach((set) => {
+                    const type = set._typeVarMap.get(key);
+                    if (type) {
+                        types.push(type);
+                    }
+                });
+                result._typeVarMap.set(key, types.length > 0 ? combineTypes(types) : undefined);
+            }
+            return result;
+        } else if (allSets.length === 1) {
+            return allSets[0];
+        } else {
+            return undefined;
+        }
+    }
 }
 
 export class ConstraintSolution {
@@ -85,5 +112,30 @@ export class ConstraintSolution {
     getSolutionSet(index: number) {
         assert(index >= 0 && index < this._solutionSets.length);
         return this._solutionSets[index];
+    }
+
+    static combine(allSolutions: ConstraintSolution[]): ConstraintSolution | undefined {
+        if (allSolutions.length > 1) {
+            const firstSolutions = allSolutions[0]._solutionSets;
+            assert(
+                allSolutions.every((constraints) => constraints._solutionSets.length === firstSolutions.length),
+                'All solutions must have the same number of sets'
+            );
+            const result = new ConstraintSolution();
+            result._solutionSets = [];
+            for (let i = 0; i < firstSolutions.length; i++) {
+                const combinedSet = ConstraintSolutionSet.combine(
+                    allSolutions.map((constraints) => constraints._solutionSets[i])
+                );
+                if (combinedSet) {
+                    result._solutionSets.push(combinedSet);
+                }
+            }
+            return result;
+        } else if (allSolutions.length === 1) {
+            return allSolutions[0];
+        } else {
+            return undefined;
+        }
     }
 }
